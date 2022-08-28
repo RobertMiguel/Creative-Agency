377991f48be6d6bc3d433e78106fd86bc618ab79
Robertin do Gelo ❄️ <90520597+RobertMiguel@users.noreply.github.com>
2022-08-28 02:41:00 -0300

    Creative Agency -  protect.php

--- GitHub/Creative Agency/protect.php
+++ GitHub/Creative Agency/protect.php
@@ -4,6 +4,7 @@ if(!isset($_SESSION)) {
     session_start();
 }
 
+
 if(!isset($_SESSION['id'])) {
     die("Você não pode acessar esta página porque não está logado.<p><a href=\"index.php\">Entrar</a></p>");
 }
