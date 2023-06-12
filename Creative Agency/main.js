35e575b5c150cdbac3eeba2cd2a6e730ef8abb7d
RobertMiguel <90520597+RobertMiguel@users.noreply.github.com>
2023-06-11 22:56:04 -0300

    Atualizar o main.js

--- Creative Agency/main.js
+++ Creative Agency/main.js
@@ -8,7 +8,6 @@ const closeModal = () => {
     document.getElementById('modal').classList.remove('active')
 }
 
-
 const getLocalStorage = () => JSON.parse(localStorage.getItem('db_client')) ?? []
 const setLocalStorage = (dbClient) => localStorage.setItem("db_client", JSON.stringify(dbClient))
 
